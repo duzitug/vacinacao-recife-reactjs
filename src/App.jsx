2300e242fb2ad39318f2836b64d05760ad7d1dce
Mércio Andrade Filho <merciofilho@gmail.com>
2022-01-17 10:57:19 -0300

    add ControlledOpenSelect component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom";
+import ControlledOpenSelect from "./ControlledOpenSelect";
+import { getLocaisVacinacaoRecife } from "./data";
 
 function App() {
-  React.useEffect(function () {}, []);
+  const [locaisVacinacaoRecife, setLocaisVacinacaoRecife] = React.useState([]);
+
+  // criar array com os nomes dos bairros sem repetição
+
+  React.useEffect(function () {
+    const locaisVacinacao = getLocaisVacinacaoRecife();
+
+    const bairros = [];
+
+    locaisVacinacao.forEach(function (local) {
+      if (bairros.includes(local.bairro));
+      else bairros.push(local.bairro);
+    });
+
+    console.log(bairros);
+
+    setLocaisVacinacaoRecife(getLocaisVacinacaoRecife());
+  }, []);
 
   return (
     <>
-      <h1>Hello CodeSandbox</h1>
+      <h1>Componente App</h1>
+
+      <ControlledOpenSelect />
+
+      {locaisVacinacaoRecife.map((local) => (
+        <h6> {local.bairro} </h6>
+      ))}
     </>
   );
 }
