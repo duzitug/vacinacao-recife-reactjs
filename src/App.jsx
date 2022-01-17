2d3ea17150e9c3a2da506aad72799a667bb260ce
Mércio Andrade Filho <merciofilho@gmail.com>
2022-01-17 08:53:16 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  React.useEffect(function () {}, []);
+
+  return (
+    <>
+      <h1>Hello CodeSandbox</h1>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, window.document.getElementById("root"));
---
+++ src/data.js
@@ -0,0 +1,439 @@
+export function getLocaisVacinacaoRecife() {
+  return [
+    {
+      _id: 1,
+      Local: "Centro de Vacinação - Compaz Ariano Suassuna (Centro)",
+      logradouro: "Av. Gen. San Martin, 1208",
+      bairro: "Cordeiro",
+      fone: 32259400,
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 18:30",
+      latitude: -8.0596332,
+      longitude: -34.9260596
+    },
+    {
+      _id: 2,
+      Local: "Centro de Vacinação - DNOCS",
+      logradouro: "R. Cônego Barata, 999",
+      bairro: "Tamarineira",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 18:30",
+      latitude: -8.02759,
+      longitude: -34.8987
+    },
+    {
+      _id: 3,
+      Local: "Centro de Vacinação – Geraldão (Centro)",
+      logradouro: "Av. Mal. Mascarenhas de Morais, 7787",
+      bairro: "Imbiribeira",
+      fone: 34977455,
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 18:30",
+      latitude: -8.1169725,
+      longitude: -34.9132368
+    },
+    {
+      _id: 4,
+      Local: "Centro de Vacinação - Parque da Macaxeira",
+      logradouro: "Av. Norte Miguel Arraes de Alencar, S/N",
+      bairro: "Macaxeira",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 18:30",
+      latitude: -8.01571,
+      longitude: -34.9312
+    },
+    {
+      _id: 5,
+      Local:
+        "Centro de Vacinação - UFRPE Universidade Federal Rural de Pernmbuco (Centro)",
+      logradouro:
+        "Rua Dom Manuel de Medeiros - Departamento de Agronomia - Entrada Z04",
+      bairro: "Dois Irmãos",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 18:30",
+      latitude: -8.01816,
+      longitude: -34.9525
+    },
+    {
+      _id: 6,
+      Local: "Centro de Vacinação - Upa-E Ibura (Centro)",
+      logradouro: "Av. Dois Rios, 170",
+      bairro: "Ibura",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 18:30",
+      latitude: -8.109992,
+      longitude: -34.936492
+    },
+    {
+      _id: 7,
+      Local: "Centro de Vacinação - UNICAP",
+      logradouro: "Rua do Príncipe, 526",
+      bairro: "Boa Vista",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 18:30",
+      latitude: -8.0556667,
+      longitude: -34.8881208
+    },
+    {
+      _id: 8,
+      Local: "Drive Thru -  Geraldão",
+      logradouro: "Av. Mal. Mascarenhas de Morais, 7787",
+      bairro: "Imbiribeira",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.1169725,
+      longitude: -34.9132368
+    },
+    {
+      _id: 9,
+      Local: "Drive Thru -  Justiça Federal - Av. Recife",
+      logradouro: "Av. Recife, Fúrum Ministro Artur Marinho, 6250",
+      bairro: "Jiquiá",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.07285,
+      longitude: -34.9355
+    },
+    {
+      _id: 10,
+      Local: "Drive Thru - DNOCS",
+      logradouro: "R. Cônego Barata, 999",
+      bairro: "Tamarineira",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.02759,
+      longitude: -34.8987
+    },
+    {
+      _id: 11,
+      Local: "Drive Thru - Parque da Macaxeira",
+      logradouro: "Av. Norte Miguel Arraes de Alencar, S/N",
+      bairro: "Macaxeira",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.01571,
+      longitude: -34.9312
+    },
+    {
+      _id: 12,
+      Local: "Drive Thru - UFPE Universidade Federal de Pernambuco",
+      logradouro: "Av. dos Funcionários, S/N  - CFCH/UFPE",
+      bairro: "Cidade Universitária",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.049941,
+      longitude: -34.953153
+    },
+    {
+      _id: 13,
+      Local: "Drive Thru - UFRPE Universidade Federal Rural de Pernambuco",
+      logradouro:
+        "Rua Dom Manuel de Medeiros - Departamento de Agronomia - Entrada Z04",
+      bairro: "Dois Irmãos",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.01816,
+      longitude: -34.9525
+    },
+    {
+      _id: 14,
+      Local: "Drive Thru - UNICAP",
+      logradouro: "Rua do Príncipe, 526",
+      bairro: "Boa Vista",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.0556667,
+      longitude: -34.8881208
+    },
+    {
+      _id: 15,
+      Local: "Upinha Nossa Senhora do Pilar",
+      logradouro: "Rua do Brum",
+      bairro: "Recife",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.05697346195763,
+      longitude: -34.8713332944197
+    },
+    {
+      _id: 16,
+      Local: "USF Coque Berilo Pernambucano",
+      logradouro: "Rua Guapirama, 65",
+      bairro: "Joana Bezerra",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.07148476506564,
+      longitude: -34.899954918008
+    },
+    {
+      _id: 17,
+      Local: "Policlínica Gouveia de Barros",
+      logradouro: "Largo da Santa Cruz, s/n",
+      bairro: "Boa Vista",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.06204433315009,
+      longitude: -34.8881462161437
+    },
+    {
+      _id: 18,
+      Local: "Policlínica Amaury Coutinho",
+      logradouro: "Rua Iguatu, s/n",
+      bairro: "Campina do Barreto",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.01591838051985,
+      longitude: -34.8822682700118
+    },
+    {
+      _id: 19,
+      Local: "Policlínica Salomão Kelner",
+      logradouro: "Estrada Velha de água Fria, 1652",
+      bairro: "água Fria",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.02093710976986,
+      longitude: -34.8957976221739
+    },
+    {
+      _id: 20,
+      Local: "UBT Professor Monteiro de Moraes",
+      logradouro: "Av. Beberibe, 4510",
+      bairro: "Beberibe",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.00400313848613,
+      longitude: -34.89678942784
+    },
+    {
+      _id: 21,
+      Local: "Policlínica Albert Sabin",
+      logradouro: "Rua Padre Roma, 191",
+      bairro: "Tamarineira",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.03396114208128,
+      longitude: -34.9053587107149
+    },
+    {
+      _id: 22,
+      Local: "UBT Francisco Pignatari",
+      logradouro: "Estrada do Arraial, 4155",
+      bairro: "Casa Amarela",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.02756929169843,
+      longitude: -34.9187238192978
+    },
+    {
+      _id: 23,
+      Local: "USF Sítio dos Pintos",
+      logradouro: "Rua Pref. Agostinho Nunes Machado, 26",
+      bairro: "Sítio dos Pintos",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.01383980649463,
+      longitude: -34.9578423705808
+    },
+    {
+      _id: 24,
+      Local: "UBT José Dustan",
+      logradouro: "Av. Mauricio de Nassau, s/n, Caiara",
+      bairro: "Iputinga",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.03861599059897,
+      longitude: -34.9309338443817
+    },
+    {
+      _id: 25,
+      Local: "UBT Joaquim Cavalcanti",
+      logradouro: "Av. Estrada do Forte do Arraial Novo do Bom Jesus, 1350",
+      bairro: "Torrões",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.05897060905633,
+      longitude: -34.9318664109021
+    },
+    {
+      _id: 26,
+      Local: "Upinha Vila Arraes",
+      logradouro: "Av. Afonso Olindense, 1513",
+      bairro: "Várzea",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.04662928138865,
+      longitude: -34.9593550349962
+    },
+    {
+      _id: 27,
+      Local: "Centro de Saúde Romero Marques",
+      logradouro: "Rua Carlos Gomes, 932",
+      bairro: "Prado",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.06381932806925,
+      longitude: -34.9136717169509
+    },
+    {
+      _id: 28,
+      Local: "Centro de Saúde Gaspar Regueira",
+      logradouro: "Rua Padre Diogo Rodrigues, s/n",
+      bairro: "Barro",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.08985129127221,
+      longitude: -34.9421824130236
+    },
+    {
+      _id: 29,
+      Local: "Centro de Saúde Bidu Krause",
+      logradouro: "3A Travessa Onze de Agosto",
+      bairro: "Curado",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.07966652244597,
+      longitude: -34.970261064704
+    },
+    {
+      _id: 30,
+      Local: "UBT djair Brindeiro",
+      logradouro: "Rua Cosmorama, s/n",
+      bairro: "Boa Viagem",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.14816750750721,
+      longitude: -34.9137073237242
+    },
+    {
+      _id: 31,
+      Local: "Policlínicado Pina",
+      logradouro: "Av. República do Líbano, 355",
+      bairro: "Pina",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.08590719203129,
+      longitude: -34.8904455437866
+    },
+    {
+      _id: 32,
+      Local: "Centro de Saúde Iná Rosa Borges",
+      logradouro: "Rua Sen. Milton de Campos, 59-1",
+      bairro: "Vasco da Gama",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.01623778186682,
+      longitude: -34.9210922534473
+    },
+    {
+      _id: 33,
+      Local: "Centro deSaúdeBruno Maia",
+      logradouro: "Rua Alto do Refúgio",
+      bairro: "Nova Descoberta",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.00137934676975,
+      longitude: -34.9247766359165
+    },
+    {
+      _id: 34,
+      Local: "Centro de Saúde Hélio Mendonça",
+      logradouro: "Rua Erundina Negreiros de Araújo",
+      bairro: "Córrego do Jenipapo",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.00678885984594,
+      longitude: -34.9374247837472
+    },
+    {
+      _id: 35,
+      Local: "UBT Ivo Rabelo",
+      logradouro: "Av. Campina Grande, 199",
+      bairro: "Cohab",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.11963543141028,
+      longitude: -34.9479155098662
+    },
+    {
+      _id: 36,
+      Local: "Upinha UR 04/05",
+      logradouro: "Rua Cap. Vicente Curado, 33",
+      bairro: "Cohab",
+      fone: "",
+      como_usar:
+        "Os usuários devem levar um documento oficial de identificação e documentos comprobatórios e/ou comprovante de residência em seu nome, quando for necessário. Nos casos de 2a dose, levar documento e o cartão de vacinação.",
+      horario: "Domingo à Domingo das 07:30 às 17:30",
+      latitude: -8.13202094021881,
+      longitude: -34.9482867963315
+    }
+  ];
+}
